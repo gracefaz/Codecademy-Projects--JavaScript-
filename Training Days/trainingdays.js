e2e2708682518f5b12d9ab58e4c5a3bcb4317318
gracefaz <gracefaz27@gmail.com>
2022-02-12 12:36:30 +0000

    Training Days Project

---
+++ Training Days/trainingdays.js
@@ -0,0 +1,52 @@
+// Fixed the scope of the code to make it run more smoothly and logically. Code now works.
+
+const getRandEvent = () => {
+  const random = Math.floor(Math.random() * 3);
+  if (random === 0) {
+    return 'Marathon';
+  } else if (random === 1) {
+    return 'Triathlon';
+  } else if (random === 2) {
+    return 'Pentathlon';
+  }
+};
+
+const getTrainingDays = event => {
+  let days
+  if (event === 'Marathon') {
+    days = 50;
+  } else if (event === 'Triathlon') {
+    days = 100;
+  } else if (event === 'Pentathlon') {
+    days = 200;
+  }
+
+  return days;
+};
+
+// Moved the name variable to the global space.
+const name = 'Nala';
+
+// Let the name variable ba an argument of the logEvent and logTime functions.
+const logEvent = (name, event) => {
+  console.log(`${name}'s event is: ${event}`);
+};
+
+const logTime = (name, days) => {
+  console.log(`${name}'s time to train is: ${days} days`);
+};
+
+const event = getRandEvent();
+const days = getTrainingDays(event);
+
+// Define a `name` variable. Use it as an argument after updating logEvent and logTime 
+logEvent(name, event);
+logTime(name, days);
+
+// Copy and pasted from Codecademy to add another person and see if the code runs as we want it to.
+const event2 = getRandEvent();
+const days2 = getTrainingDays(event2);
+const name2 = 'Warren';
+ 
+logEvent(name2, event2);
+logTime(name2, days2);
